a6b3546455784ed5123f8fce2c2bd92e2b54e414
vicmanbrile <victor.briseno.leon@hotmail.com>
2021-07-12 14:39:47 +0000

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import './styles/Header.css'
+
+function Header() {
+    return (
+      <div className="Header">
+        <ul>
+            <li>Home</li>
+            <li>Proyects</li>
+            <li>Social</li>
+        </ul>
+      </div>
+    );
+  }
+
+  export default Header;  
